61a889156dcf329bcd006727385aed57bf7b6844
clarkenj <natashajayne@gmail.com>
2024-03-18 15:48:38 -0400

    updated regex

--- code/count_frames.py
+++ code/count_frames.py
@@ -11,7 +11,9 @@ def count_frames(hdf5_object, target_suffix="atlas-MIST_desc-64_timeseries", pat
         item_path = f"{path}/{name}" if path else name
         if isinstance(hdf5_object[name], h5py.Dataset) and name.endswith(target_suffix):
             # Collect dataset name and the first dimension of its shape
-            data.append({"file": name, "n_frames": hdf5_object[name].shape[0]})
+            data.append(
+                {"file": name, "total_frames": hdf5_object[name].shape[0]}
+            )  # n_frames
         elif isinstance(hdf5_object[name], h5py.Group):
             # Recursive call to navigate through groups and collect data
             data.extend(count_frames(hdf5_object[name], target_suffix, item_path))
@@ -45,7 +47,7 @@ if __name__ == "__main__":
             if sub_directory.is_dir() and sub_directory.name != "working_directory":
                 hdf5_file_p = (
                     sub_directory
-                    / f"sub-{sub_directory.name}_atlas-MIST_desc-scrubbing.5+gsr.h5"
+                    / f"sub-{sub_directory.name}_atlas-MIST_desc-simple.h5"  # atlas-MIST_desc-scrubbing.5+gsr.h5"
                 )
                 if hdf5_file_p.exists():
                     with h5py.File(hdf5_file_p, "r") as file:
@@ -53,7 +55,9 @@ if __name__ == "__main__":
                 else:
                     print(f"File {hdf5_file_p} does not exist.")
     elif args.level == "group":
-        hdf5_file_p = args.input_p / "atlas-MIST_desc-scrubbing.5+gsr.h5"
+        hdf5_file_p = (
+            args.input_p / "atlas-MIST_desc-simple.h5"
+        )  # "atlas-MIST_desc-scrubbing.5+gsr.h5"
         with h5py.File(hdf5_file_p, "r") as file:
             data = count_frames(file)
 
@@ -62,13 +66,15 @@ if __name__ == "__main__":
 
     # Extract file details. Session and run are optional
     pattern = (
-        r"sub-([^\s/_]+)_task-([a-zA-Z0-9]+)(?:_ses-([a-zA-Z0-9]+))?(?:_run-(\d+))?"
+        r"sub-([^\s/_]+)(?:_ses-([a-zA-Z0-9]+))?_task-([a-zA-Z0-9]+)(?:_run-(\d+))?"
     )
     df[["participant_id", "ses", "task", "run"]] = df["file"].str.extract(pattern)
+    df["participant_id"] = df["participant_id"].astype(str)
 
     # Drop the file column and add dataset as a column
     df.drop(columns=["file"], inplace=True)
     df["dataset"] = args.dataset
 
     # Save output
-    df.to_csv(args.output_p / f"{args.dataset}_frames.tsv", sep="\t", index=False)
+    # df.to_csv(args.output_p / f"{args.dataset}_frames.tsv", sep="\t", index=False)
+    df.to_csv(args.output_p / f"{args.dataset}_frames_total.tsv", sep="\t", index=False)
